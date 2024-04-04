49f30d9ed793a88d8678680487c2025569441d29
jo-shua33 <126611915+jo-shua33@users.noreply.github.com>
2024-04-04 15:15:44 -0700

    seventh commit-done

--- script.js
+++ script.js
@@ -45,26 +45,23 @@ function validateForm() {
   let atposition = emaill.indexOf("@");
   let dotposition = emaill.lastIndexOf(".");
 
-  if (
+  if (name == null || name == "") {
+    alert("Name cannot be blank.");
+  } else if (
     atposition < 1 ||
     dotposition < atposition + 2 ||
     dotposition + 2 >= emaill.length
   ) {
-    alert("please enter a valid email address.");
-  } else if (name == null || name == "") {
-    alert("name cannot be blank!");
+    alert("Please enter a valid email address.");
   } else if (password.length < 6) {
-    alert("password must be atleast 6 characters long.");
-  } else {
-    alert("successfully created an account.");
+    alert("Password must be atleast 6 characters long.");
+  } else if ((name !== "") & (emaill !== "")) {
+    document.myForm.reset();
+    alert("Successfully signed up.");
   }
   return false;
 }
 
-if ((name !== "") & (emaill !== "")) & (password.length > 6) {
-  document.myForm.reset();
-  alert("form submitted successfully.");
-}
 function validateEmail() {
   let emaill = document.myForm.email.value;
   let password = document.myForm.password.value;
@@ -76,11 +73,12 @@ function validateEmail() {
     dotposition < atposition + 2 ||
     dotposition + 2 >= emaill.length
   ) {
-    alert("please enter a valid email address.");
+    alert("Please enter a valid email address.");
   } else if (password.length < 6) {
-    alert("password must be atleast 6 characters long.");
-  } else {
-    alert("successfully signed in.");
+    alert("Password must be atleast 6 characters long.");
+  } else if (emaill !== "") {
+    document.myForm.reset();
+    alert("Successfully signed in");
   }
   return false;
 }
