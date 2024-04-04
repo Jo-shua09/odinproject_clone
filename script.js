54a06f58563d241a7c0e736cdebdec00daecba13
jo-shua33 <126611915+jo-shua33@users.noreply.github.com>
2024-04-04 14:20:28 -0700

    sixth commit

--- script.js
+++ script.js
@@ -35,3 +35,53 @@ navClose.forEach(function (element) {
 });
 
 // NAVBAR SETTINGS
+
+// FORM VALIDATION
+
+function validateForm() {
+  let name = document.myForm.name.value;
+  let emaill = document.myForm.email.value;
+  let password = document.myForm.password.value;
+  let atposition = emaill.indexOf("@");
+  let dotposition = emaill.lastIndexOf(".");
+
+  if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= emaill.length
+  ) {
+    alert("please enter a valid email address.");
+  } else if (name == null || name == "") {
+    alert("name cannot be blank!");
+  } else if (password.length < 6) {
+    alert("password must be atleast 6 characters long.");
+  } else {
+    alert("successfully created an account.");
+  }
+  return false;
+}
+
+if ((name !== "") & (emaill !== "")) & (password.length > 6) {
+  document.myForm.reset();
+  alert("form submitted successfully.");
+}
+function validateEmail() {
+  let emaill = document.myForm.email.value;
+  let password = document.myForm.password.value;
+  let atposition = emaill.indexOf("@");
+  let dotposition = emaill.lastIndexOf(".");
+
+  if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= emaill.length
+  ) {
+    alert("please enter a valid email address.");
+  } else if (password.length < 6) {
+    alert("password must be atleast 6 characters long.");
+  } else {
+    alert("successfully signed in.");
+  }
+  return false;
+}
+// FORM VALIDATION
